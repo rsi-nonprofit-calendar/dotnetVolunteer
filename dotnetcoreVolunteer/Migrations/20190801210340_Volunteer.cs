8ee19a2f6426dc113fde388490bcfea4d759c190
fmunoz111 <38711668+fmunoz111@users.noreply.github.com>
2019-08-01 15:05:36 -0600

    added models for Achievements, Picture, Favorite, and Volunteer.  Migrations were made for each one.

---
+++ dotnetcoreVolunteer/Migrations/20190801210340_Volunteer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnetcoreVolunteer.Migrations
+{
+    public partial class Volunteer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
